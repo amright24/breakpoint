657abbea0ad56791c65eaeffbb3dc6147209d4b1
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-03-28 13:06:49 -0400

    configured feedcell

---
+++ breakpoint/Views/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by Austin Rightnowar on 3/28/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+    
+}
