9846bac822ce9602364cc2025055506b0c8f4a96
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-03-29 17:01:50 -0400

    added group search functionality so it can reload query whenever its updated

---
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,71 @@
+//
+//  CreateGroupsVC.swift
+//  breakpoint
+//
+//  Created by Austin Rightnowar on 3/29/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+    
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearchTextField: InsetTextField!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    
+    var emailArray = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        emailSearchTextField.delegate = self
+        emailSearchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange() {
+        if emailSearchTextField.text == "" {
+            emailArray = []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmail(forSearchQuery: emailSearchTextField.text!) { (returnedEmailArray) in
+                self.emailArray = returnedEmailArray
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emailArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        
+        cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: true)
+        return cell
+    }
+    
+}
+
+extension CreateGroupsVC: UITextFieldDelegate {
+    
+}
